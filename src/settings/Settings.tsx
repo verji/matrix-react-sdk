a88b986057ba2786995b0e93a21b777d4030e677
Ståle Eikebråten <eiksta@verji.com>
2024-03-22 11:53:20 +0100

    UI-features v3

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -1259,7 +1259,7 @@ export const SETTINGS: { [setting: string]: ISetting } = {
         supportedLevels: LEVELS_UI_FEATURE,
         default: true,
     },
-   [UIFeature.EmailAddressShowRemoveButton]: {
+    [UIFeature.EmailAddressShowRemoveButton]: {
         supportedLevels: LEVELS_UI_FEATURE,
         default: true,
     },
