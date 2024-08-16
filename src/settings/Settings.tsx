37d20ed97369f073b8edf55fc5687ec7bcd475e6
Ståle Eikebråten <eiksta@verji.com>
2024-08-16 12:15:33 +0200

    1991, 1992 hide labs and link dm invite

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -208,7 +208,7 @@ export type ISetting = IBaseSetting | IFeature;
 
 export const SETTINGS: { [setting: string]: ISetting } = {
     "feature_video_rooms": {
-        isFeature: false,
+        isFeature: false, //Verji
         labsGroup: LabGroup.VoiceAndVideo,
         displayName: _td("labs|video_rooms"),
         supportedLevels: LEVELS_DEVICE_ONLY_SETTINGS_WITH_CONFIG,
