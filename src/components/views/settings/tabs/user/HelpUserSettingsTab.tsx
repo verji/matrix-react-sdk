9413869d2bb200a4c2accd0d0ac664fc0b77e36f
Ståle Eikebråten <eiksta@verji.com>
2024-08-23 14:03:40 +0200

    Fixed lint errors

--- src/components/views/settings/tabs/user/HelpUserSettingsTab.tsx
+++ src/components/views/settings/tabs/user/HelpUserSettingsTab.tsx
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React, { ReactNode } from "react";
+// import React, { ReactNode } from "react";
+import React from "react";
 import { logger } from "matrix-js-sdk/src/logger";
 import {
     CustomComponentLifecycle,
